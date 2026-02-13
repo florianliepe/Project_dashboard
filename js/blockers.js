86fc5abe3d09fa1bb441330414e037f89f139967
florianliepe <florian.liepe@eraneos.com>
2026-02-13 22:20:04 +0100

    Clean up comments in blockers.js
    Remove outdated comments regarding the Blockers sheet structure.

--- js/blockers.js
+++ js/blockers.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
     'use strict';
     // This is the full and final code for blockers.js
-    // All functions are included and mapped to the new "Blockers" sheet structure.
 });
